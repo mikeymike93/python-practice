2b0a1a6af72107ded631447ab9d7d3a897ba8e17
Michael Sena <msena8971@gmail.com>
2026-08-05 19:50:52 -0700

    Add pass/fail

--- grade_calculator.py
+++ grade_calculator.py
@@ -6,4 +6,20 @@ grade3= float(input("Enter the third grade: "))
 
 average= (grade1 + grade2 + grade3) / 3
 
-print(f"{name}'s average grade is {average:.2f}")
+if average >=90:
+    letter_grade= "A"
+elif average >=80:
+    letter_grade= "B"
+elif average >=70:
+    letter_grade= "C"
+elif average >=60:
+    letter_grade= "D"
+else:
+    letter_grade= "F"
+
+if average < 60:
+    status= "failing"
+else:
+    status= "passing"
+
+print(f"{name}'s average grade is {average:.2f}, which is a {letter_grade}, and they are {status}.")
